0f7bf297e9336f19a28fa4720bee3df095d73fcf
Deissei <privetizkyrgyzstana2@gmail.com>
2023-08-01 15:54:57 +0600

    update project, add filters on nft

--- apps/nft/views.py
+++ apps/nft/views.py
@@ -1,13 +1,18 @@
-from rest_framework import permissions, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, permissions, viewsets
 
 from apps.nft.models import Nft
-from apps.nft.serializers import NftSerializer, NftLISTSerializer
+from apps.nft.serializers import NftLISTSerializer, NftSerializer
+from utils.filters import NftPriceFilter
 from utils.permissions import IsCollectionNFTOwner
 
 
 class NftAPIViewSet(viewsets.ModelViewSet):
     queryset = Nft.objects.all()
     serializer_class = NftSerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ("title", "author__username", "collection_id__title")
+    filterset_class = NftPriceFilter
 
     def get_permissions(self):
         if self.action in ('update', 'destroy'):
